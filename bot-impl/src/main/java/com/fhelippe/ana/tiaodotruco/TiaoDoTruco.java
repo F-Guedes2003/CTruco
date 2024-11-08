286be6ff65b117869c85254e2689d5732e34432e
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-08 14:02:59 -0300

    feat: hasCopas and hasZap created

--- bot-impl/src/main/java/com/fhelippe/ana/tiaodotruco/TiaoDoTruco.java
+++ bot-impl/src/main/java/com/fhelippe/ana/tiaodotruco/TiaoDoTruco.java
@@ -9,6 +9,8 @@ public class TiaoDoTruco implements BotServiceProvider {
 
     @Override
     public boolean getMaoDeOnzeResponse(GameIntel intel) {
+        if(hasZap(intel) && hasCopas(intel)) return true;
+
         if(intel.getOpponentScore() < 4 && handStrength(intel) > 26) return true;
 
         if(intel.getOpponentScore() < 5 && handStrength(intel) > 18 && hasManilha(intel)) return true;
@@ -18,7 +20,12 @@ public class TiaoDoTruco implements BotServiceProvider {
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
-        return false;
+
+        if(hasCopas(intel) && hasZap(intel)) return true;
+
+        if(handStrength(intel) > 25 && intel.getOpponentScore() < 4) return true;
+
+        return handStrength(intel) > 35;
     }
 
     @Override
@@ -41,13 +48,25 @@ public class TiaoDoTruco implements BotServiceProvider {
                 .anyMatch(e -> e.isManilha(intel.getVira()));
     }
 
+    public boolean hasZap(GameIntel intel) {
+        return intel.getCards()
+                .stream()
+                .anyMatch(e -> e.isZap(intel.getVira()));
+    }
+
+    public boolean hasCopas(GameIntel intel) {
+        return intel.getCards()
+                .stream()
+                .anyMatch(e -> e.isCopas(intel.getVira()));
+    }
+
     public double handStrength(GameIntel intel) {
 
         return intel.getCards().stream()
                 .mapToDouble(e -> {
                     double strenght = 0;
                     if( e.isZap(intel.getVira()) ) return 20;
-                    if( e.isZap(intel.getVira()) ) return 15;
+                    if( e.isCopas(intel.getVira()) ) return 15;
                     if( e.isEspadilha(intel.getVira()) ) return 13;
                     if( e.isOuros(intel.getVira()) ) return 12;
 
